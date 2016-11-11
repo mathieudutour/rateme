a8f58ebdb86df72809588a89a0c3105e624cfd84
Mathieu Dutour <mathieu@dutour.me>
2016-11-11 16:25:38 +0000

    Initial Commit

---
+++ rateme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rateme
+//
+//  Created by Mathieu Dutour on 11/11/2016.
+//  Copyright © 2016 Mathieu Dutour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
